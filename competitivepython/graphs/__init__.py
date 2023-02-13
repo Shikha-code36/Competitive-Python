6c7c29b3958fe58005238693bc318604f462549a
Shikha-code36 <pandeyshikha075@gmail.com>
2023-02-13 20:18:08 +0530

    restructure

--- competitivepython/graphs/__init__.py
+++ competitivepython/graphs/__init__.py
@@ -1,7 +1,7 @@
 # __init__.py
 from .bfs import breadth_first_search
 from .dfs import depth_first_search
-from .dijkstra import dijkstra
+from .dijkstra_shortest_path import dijkstra
 
 __all__ = ['breadth_first_search', 'depth_first_search', 'dijkstra']
 
