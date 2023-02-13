bdceb338c2218db842ef6faac425fc6e9ff66405
Shikha-code36 <pandeyshikha075@gmail.com>
2023-02-13 19:07:42 +0530

    logging in trees

--- competitivepython/trees/binary_search_tree.py
+++ competitivepython/trees/binary_search_tree.py
@@ -1,3 +1,5 @@
+import logging
+
 class Node:
     def __init__(self, value):
         self.left = None
@@ -9,43 +11,59 @@ class BinarySearchTree:
         self.root = None
 
     def insert(self, value):
-        new_node = Node(value)
-        if self.root is None:
-            self.root = new_node
-        else:
-            current_node = self.root
-            while True:
-                if value < current_node.value:
-                    if current_node.left is None:
-                        current_node.left = new_node
-                        break
+        try:
+            new_node = Node(value)
+            if self.root is None:
+                self.root = new_node
+            else:
+                current_node = self.root
+                while True:
+                    if value < current_node.value:
+                        if current_node.left is None:
+                            current_node.left = new_node
+                            break
+                        else:
+                            current_node = current_node.left
                     else:
-                        current_node = current_node.left
-                else:
-                    if current_node.right is None:
-                        current_node.right = new_node
-                        break
-                    else:
-                        current_node = current_node.right
+                        if current_node.right is None:
+                            current_node.right = new_node
+                            break
+                        else:
+                            current_node = current_node.right
+        except Exception as e:
+            logging.error(f"Error in insert: {e}")
+            return "An error occurred during insert in binary search tree: {}".format(str(e))
 
     def search(self, value):
-        current_node = self.root
-        while current_node is not None:
-            if value == current_node.value:
-                return True
-            elif value < current_node.value:
-                current_node = current_node.left
-            else:
-                current_node = current_node.right
-        return False
+        try:
+            current_node = self.root
+            while current_node is not None:
+                if value == current_node.value:
+                    return True
+                elif value < current_node.value:
+                    current_node = current_node.left
+                else:
+                    current_node = current_node.right
+            return False
+        except Exception as e:
+            logging.error(f"Error in search: {e}")
+            return "An error occurred during search in binary search tree: {}".format(str(e))
 
     def in_order_traversal(self, node, result):
-        if node is not None:
-            self.in_order_traversal(node.left, result)
-            result.append(node.value)
-            self.in_order_traversal(node.right, result)
+        try:
+            if node is not None:
+                self.in_order_traversal(node.left, result)
+                result.append(node.value)
+                self.in_order_traversal(node.right, result)
+        except Exception as e:
+            logging.error(f"Error in in_order_traversal: {e}")
+            return "An error occurred during in order traversal in binary search tree: {}".format(str(e))
 
     def get_in_order_traversal(self):
-        result = []
-        self.in_order_traversal(self.root, result)
-        return result
+        try:
+            result = []
+            self.in_order_traversal(self.root, result)
+            return result
+        except Exception as e:
+            logging.error(f"Error in get_in_order_traversal: {e}")
+            return "An error occurred during in order traversal in binary search tree: {}".format(str(e))
